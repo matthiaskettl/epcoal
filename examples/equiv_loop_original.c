6321076c9a72db42853292934188d311cd51636e
Matthias Kettl <matthias.kettl@sosy.ifi.lmu.de>
2026-03-27 13:10:54 +0100

    Add examples

---
+++ examples/equiv_loop_original.c
@@ -0,0 +1,52 @@
+extern void abort(void);
+extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+void reach_error() { __assert_fail("0", "diamond_2-1.c", 3, "reach_error"); }
+extern unsigned int __VERIFIER_nondet_uint(void);
+
+typedef unsigned int uint;
+
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+    ERROR: {reach_error();abort();}
+  }
+  return;
+}
+
+int main(void) {
+  uint x = 0;
+  uint y = __VERIFIER_nondet_uint();
+
+  while (x < 99) {
+    if (y % 2 == 0) x++;
+    else x += 2;
+
+    if (y % 2 == 0) x += 2;
+    else x -= 2;
+
+    if (y % 2 == 0) x += 2;
+    else x += 2;
+
+    if (y % 2 == 0) x += 2;
+    else x -= 2;
+
+    if (y % 2 == 0) x += 2;
+    else x += 2;
+
+    if (y % 2 == 0) x += 2;
+    else x -= 4;
+
+    if (y % 2 == 0) x += 2;
+    else x += 4;
+
+    if (y % 2 == 0) x += 2;
+    else x += 2;
+
+    if (y % 2 == 0) x += 2;
+    else x -= 4;
+
+    if (y % 2 == 0) x += 2;
+    else x -= 4;
+  }
+
+  __VERIFIER_assert((x % 2) == (y % 2));
+}
---
+++ examples/example_m.c
@@ -0,0 +1,19 @@
+extern int __VERIFIER_nondet_int();
+
+void reach_error() {  }
+
+int alpha(int x) {
+  if (x) x--;
+  while (x == 0) {int i = 0; i++; x = i;}
+  while (x == 0) {int i = 0; i++; x = i;}
+  if (x == 0) {reach_error();}
+}
+
+int main() {
+  int x = __VERIFIER_nondet_int();
+  alpha(x);
+  if (__VERIFIER_nondet_int()) x++;
+  while (x == 0) {int i = 0; i++; x = i;}
+  while (x == 0) {int i = 0; i++; x = i;}
+  if (x == 0) {reach_error();}
+}
